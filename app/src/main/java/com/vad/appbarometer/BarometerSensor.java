5e1b2eb1371f2d9dea89ab5b02f9118540ac1ce0
Vadim <gabderahmanov99@gmail.com>
2021-05-03 12:04:28 +0300

    fix gps check dialog

--- app/src/main/java/com/vad/appbarometer/BarometerSensor.java
+++
@@ -1,5 +0,0 @@
-package com.vad.appbarometer;
-
-public class BarometerSensor {
-
-}
--- app/src/main/java/com/vad/appbarometer/MainActivity.java
+++ app/src/main/java/com/vad/appbarometer/MainActivity.java
@@ -1,63 +1,67 @@
 package com.vad.appbarometer;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.vad.appbarometer.pojos.WeatherPojo;
 import com.vad.appbarometer.retrofitzone.RetrofitClient;
 import com.vad.appbarometer.utils.MathSets;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private TextView mBarText;
     private SensorManager sensorManager;
     private Sensor pressureSensor;
-    private final static int REQUEST_CODE = 445;
     private ImageView imageViewArrow;
-    private float tempPressure;
+    private LocationManager mlocationManager;
 
-    private float lat;
-    private float lon;
-
+    public static final int REQUEST_CHECK_SETTINGS = 1209;
     public static final String API_KEY = "e19089086c20c76bdc3bfbbe2a6ad29c";
+    private static final int REQUEST_CODE_PERMISSION_OVERLAY_PERMISSION = 1043;
 
-    private FusedLocationProviderClient fusedLocationProviderClient;
 
     private SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
             float[] values = sensorEvent.values;
-            tempPressure = values[0];
-            mBarText.setText(String.format("%.2f mBar", tempPressure));
-            imageViewArrow.setRotation(MathSets.getGradus(tempPressure));
-
+            visionPreasure(values[0]);
         }
 
         @Override
@@ -66,92 +70,174 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
     private void checkPermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            getGPSdata();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)&&(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            new AlertDialog.Builder(this).setTitle("Permission Requires").setMessage("Permission ACCESS_FINE_LOCATION")
+                    .setPositiveButton("Granted", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_PERMISSION_OVERLAY_PERMISSION);
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_PERMISSION_OVERLAY_PERMISSION);
+                        }
+                    }).setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }else{
+            displayLocationSettingsRequest(this, MainActivity.this);
         }
+
     }
 
-    private void getGPSdata() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                Location location = task.getResult();
-                if(location!=null){
-                    Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
-                    try {
-                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                        lat = (float) addresses.get(0).getLatitude();
-                        lon = (float) addresses.get(0).getLongitude();
-                        response();
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSION_OVERLAY_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
             }
-        });
-
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         mBarText = (TextView) findViewById(R.id.mBarText);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
         pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         imageViewArrow = (ImageView) findViewById(R.id.imageViewArrow);
-        checkPermission();
 
+        if(pressureSensor==null){
+            checkPermission();
+        }
 
     }
 
-    private void response(){
-        System.out.println(lat+" "+lon);
+    private void visionPreasure(float pres){
+        mBarText.setText(String.format("%.2f mBar", pres));
+        imageViewArrow.setRotation(MathSets.getGradus(pres));
+    }
+
+    private void response(float lat, float lon){
             RetrofitClient.getInstance().getJsonApi().getData(lat, lon, API_KEY).enqueue(new Callback<WeatherPojo>() {
                 @Override
                 public void onResponse(Call<WeatherPojo> call, Response<WeatherPojo> response) {
                     if(response.body()!=null){
                         float pressure = response.body().getMain().getPressure();
-                        System.out.println(pressure);
+                        visionPreasure(pressure);
                     }
                 }
 
                 @Override
                 public void onFailure(Call<WeatherPojo> call, Throwable t) {
                     Toast.makeText(MainActivity.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
-                    System.out.println(t.getMessage());
                 }
             });
     }
 
+
+
     @Override
     protected void onResume() {
         super.onResume();
-        if (pressureSensor != null)
+        if (pressureSensor != null){
             sensorManager.registerListener(sensorEventListener, pressureSensor, SensorManager.SENSOR_DELAY_UI);
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        if(location!=null){
+            response((float) location.getLatitude(),(float) location.getLongitude());
+        }
+
+        if(mlocationManager!=null){
+            mlocationManager.removeUpdates(this);
+        }
+
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    private void displayLocationSettingsRequest(Context context, Activity activity) {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        setLocationSetting();
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the result
+                            // in onActivityResult().
+                            status.startResolutionForResult(activity, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Toast.makeText(context, "GPS unable", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CHECK_SETTINGS){
+            setLocationSetting();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setLocationSetting(){
+        Toast.makeText(this, "access", Toast.LENGTH_SHORT).show();
+        mlocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mlocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, MainActivity.this);
     }
 }
