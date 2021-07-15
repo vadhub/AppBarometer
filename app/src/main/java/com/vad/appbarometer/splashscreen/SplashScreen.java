06239737b2caa181a792339abd36670e4d67d301
Vadim <gabderahmanov99@gmail.com>
2021-07-15 13:12:32 +0300

    add splashscreen file

---
+++ app/src/main/java/com/vad/appbarometer/splashscreen/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.vad.appbarometer.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
