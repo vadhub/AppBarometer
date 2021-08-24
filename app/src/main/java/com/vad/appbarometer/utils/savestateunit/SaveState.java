8d29b7dab94d995dbe2a4dcb2ce7c7e1104b0bf6
Vadim <gabderahmanov99@gmail.com>
2021-08-24 11:26:03 +0300

    create SaveState for save state unit

---
+++ app/src/main/java/com/vad/appbarometer/utils/savestateunit/SaveState.java
@@ -0,0 +1,31 @@
+package com.vad.appbarometer.utils.savestateunit;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
+
+public class SaveState {
+
+    private SharedPreferences prefer;
+    private Context context;
+
+    public SaveState(Context context) {
+        this.context=context;
+    }
+
+    //save state hpa or mmhg
+    public void saveStatePres(int type){
+        prefer = context.getSharedPreferences("pressure_state_app", MODE_PRIVATE);
+        SharedPreferences.Editor ed = prefer.edit();
+        ed.putInt("type_pressure_", type);
+        ed.apply();
+    }
+
+    //get state hpa or mmhg
+    public int getStatePres(){
+        prefer = context.getSharedPreferences("pressure_state_app", MODE_PRIVATE);
+        return prefer.getInt("type_pressure_", 0);
+    }
+}
