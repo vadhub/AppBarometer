c20dcf2cbc8d035fca1351944c7f2df40b49a612
Vadim <gabderahmanov99@gmail.com>
2021-08-27 15:46:19 +0300

    communication from interface with gpsdata

--- app/src/main/java/com/vad/appbarometer/screens/main/PressurePresenter.java
+++ app/src/main/java/com/vad/appbarometer/screens/main/PressurePresenter.java
@@ -4,13 +4,14 @@ package com.vad.appbarometer.screens.main;
 import android.app.Activity;
 import android.content.Context;
 import android.content.IntentSender;
-import android.location.Geocoder;
 import android.location.LocationManager;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
@@ -18,15 +19,13 @@ import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.vad.appbarometer.pojos.WeatherPojo;
 import com.vad.appbarometer.retrofitzone.RetrofitClient;
 import com.vad.appbarometer.utils.gps.GPSdata;
-
-import java.util.Arrays;
-import java.util.Locale;
+import com.vad.appbarometer.utils.requestcodes.RequestCodes;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class PressurePresenter {
+public class PressurePresenter implements PresenterView{
 
     public static final String API_KEY ="e19089086c20c76bdc3bfbbe2a6ad29c";
     private GPSdata gps;
@@ -35,17 +34,16 @@ public class PressurePresenter {
 
     private FusedLocationProviderClient fusedLocationProviderClient;
     private LocationManager mLocationManager;
-    private Geocoder geocoder;
 
     public PressurePresenter(PressureView view, Activity activity) {
         this.view=view;
         this.activity = activity;
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity);
         mLocationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-        geocoder = new Geocoder(activity, Locale.getDefault());
-        gps = new GPSdata(fusedLocationProviderClient, mLocationManager, geocoder);
+        gps = new GPSdata(fusedLocationProviderClient, mLocationManager, this);
     }
 
+    @Override
     public void response(float lat, float lon) {
         RetrofitClient.getInstance().getJsonApi().getData(lat, lon, API_KEY).enqueue(new Callback<WeatherPojo>() {
             @Override
@@ -64,15 +62,18 @@ public class PressurePresenter {
     }
 
     public void displayLocationSettingsRequest() {
-        view.getGoogleApiClient().connect();
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(gps.getLocationRequest());
+        GoogleApiClient apiClient = view.getGoogleApiClient();
+        LocationRequest location = gps.getLocationRequest();
+        apiClient.connect();
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(location);
         builder.setAlwaysShow(true);
 
-        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(view.getGoogleApiClient(), builder.build());
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(apiClient, builder.build());
         result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
             @Override
             public void onResult(LocationSettingsResult result) {
                 final Status status = result.getStatus();
+
                 switch (status.getStatusCode()) {
                     case LocationSettingsStatusCodes.SUCCESS:
                         setCoordinate();
@@ -98,9 +99,7 @@ public class PressurePresenter {
     }
 
     public void setCoordinate(){
-        float[] i = gps.getLocation();
-        System.out.println(Arrays.toString(i)+"---------------");
-        response(i[0], i[1]);
+        gps.getLocation();
     }
 
 
