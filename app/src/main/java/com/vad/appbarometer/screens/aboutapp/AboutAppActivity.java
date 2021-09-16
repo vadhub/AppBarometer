c8fdb1a4099050db71fb9b0ae5567534a81891e8
Vadim <gabderahmanov99@gmail.com>
2021-09-16 11:23:22 +0300

    add about app screen

---
+++ app/src/main/java/com/vad/appbarometer/screens/aboutapp/AboutAppActivity.java
@@ -0,0 +1,16 @@
+package com.vad.appbarometer.screens.aboutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vad.appbarometer.R;
+
+public class AboutAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+    }
+}
